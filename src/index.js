a93caf049953e057f1b21b6c9435b5545f29047f
Rahul <mrahulmaity623@gmail.com>
2023-12-22 13:15:59 +0530

    cusotm api response & err handling

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@
 import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`app running on ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongodb connection failed! ", error);
+  });
 
 /*
 import express from "express";
