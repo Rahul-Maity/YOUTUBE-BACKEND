b40eeee0d5b64c9b04bc1629ae0f1dd5a566d1b7
Rahul <mrahulmaity623@gmail.com>
2023-12-25 22:08:13 +0530

    reconfigure coverimg optional problem

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,15 +6,17 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { apiResponse } from "../utils/apiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    //get users details from frontend
-    //validation - not empty
-    //check if already exists: username,email
-    //check for images,check for files
-    //upload them to cloudinary,avatar
-    //create user object - create entry in db
-    //remove password and refresh token field from response
-    //check for user creation
-    //return res
+    /* step by step algorithm
+    get users details from frontend
+    validation - not empty
+    check if already exists: username,email
+    check for images,check for files
+    upload them to cloudinary,avatar
+    create user object - create entry in db
+    remove password and refresh token field from response
+    check for user creation
+    return res
+    */
     const { fullname, email, username, password } = req.body;
     console.log("email :", email);
     if ([fullname, email, username, password].some((field) => field?.trim() === "")) {
@@ -27,7 +29,14 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new apiError(409,"User with email or username already exists")
     }
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;-> this method give us property undefined error because we kept this property optional this field might not have come but we expect it came
+
+    //another classical if-else code to handle this situation
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
+
     if (!avatarLocalPath) {
         throw new apiError(400,"Avatar file is required")
     }
